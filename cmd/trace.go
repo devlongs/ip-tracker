c6de80e7910d681084cfeffe08f3160a99bf2210
Longs Pemun Gotar <developerlongs@gmail.com>
2023-01-20 22:16:00 +0000

    added a new command

--- cmd/trace.go
+++ cmd/trace.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -15,6 +19,7 @@ var traceCmd = &cobra.Command{
 		if len(args) > 0 {
 			for _, ip := range args {
 				fmt.Println(ip)
+				showData()
 			}
 		} else {
 			fmt.Println("Please provide an IP address to trace")
@@ -24,4 +29,45 @@ var traceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(traceCmd)
+}
+
+type Ip struct {
+	IP			string `json::"ip"`
+	City 		string `json::"city"`
+	Region 		string `json::"region"`
+	Country 	string `json::"country"`
+	Loc 		string `json::"loc"`
+	Timezone 	string `json::"timezone"`
+	Postal 		string `json::"postal"`
+}
+
+func showData(){
+	url := "http://ipinfo.io/1.1.1.1/geo"
+	responseByte := getData(url)
+
+	data := Ip{}
+
+	err := json.Unmarshal(responseByte, &data)
+	if err != nil {
+		log.Println("Unable to unmarshal the response")
+	}
+
+	fmt.Println("DATA FOUND :")
+	fmt.Println(data)
+}
+
+func getData(url string) []byte{
+	response, err := http.Get(url)
+
+	if err != nil{
+		log.Println("Unable to get response")
+	}
+
+	responseByte, err := ioutil.ReadAll(response.Body)
+
+	if err != nil{
+		log.Println("Unable to read the response")
+	}
+
+	return responseByte
 }
